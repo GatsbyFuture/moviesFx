01d2d1c4888c361f4618222644b7952e70512c23
Jack <raxmatoveldor37@gmail.com>
2025-01-20 10:13:28 +0500

    Using api for get real data from server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {v4 as uuid} from 'uuid';
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
@@ -9,7 +9,8 @@ import MoviesAddForm from "../movies-add-form/movies-add-form";
 import './app.css';
 
 const App = () => {
-    const [data, setData] = useState(dataArr);
+    const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
     const [term, setTerm] = useState("");
     const [filter, setFilter] = useState("all");
 
@@ -69,6 +70,25 @@ const App = () => {
 
     const visibleData = filterHandler(searchHandler(data, term), filter);
 
+    useEffect(() => {
+        setLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5')
+            .then(response => response.json())
+            .then(data => {
+                const newArray = data.map(item => ({
+                    id: item.id,
+                    name: item.title,
+                    viewers: Math.floor(Math.random() * 1000) + 1,
+                    favourite: Math.random() > 0.5,
+                    like: Math.random() > 0.5,
+                }))
+                // console.log(newArray);
+                setData(newArray);
+            })
+            .catch((e) => console.log(e))
+            .finally(() => setLoading(false));
+    }, []);
+
     return (
         <div className='app font-monospace'>
             <div className='content'>
@@ -85,6 +105,7 @@ const App = () => {
                         onFilter={onFilter}
                     />
                 </div>
+                {isLoading && "Loading..."}
                 <MovieList
                     data={visibleData}
                     onDelete={onDelete}
@@ -97,10 +118,10 @@ const App = () => {
 
 }
 
-const dataArr = [
-    {name: "Jerry Macguary (Tom cruise)", viewers: 350, favourite: true, like: false, id: 1},
-    {name: "Tomorrow of adge", viewers: 375, favourite: false, like: false, id: 2},
-    {name: "Impossible mission (Tom cruise).", viewers: 753, favourite: false, like: true, id: 3}
-];
+// const dataArr = [
+//     {name: "Jerry Macguary (Tom cruise)", viewers: 350, favourite: true, like: false, id: 1},
+//     {name: "Tomorrow of adge", viewers: 375, favourite: false, like: false, id: 2},
+//     {name: "Impossible mission (Tom cruise).", viewers: 753, favourite: false, like: true, id: 3}
+// ];
 
 export default App;
